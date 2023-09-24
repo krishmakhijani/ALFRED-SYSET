ba2350aea682450c37cda6e673290e447d09c1b8
Krish Makhijani <112251957+krishmakhijani@users.noreply.github.com>
2023-09-24 15:38:35 +0530

    ALFRED SYS

--- Alfred/Alfred.alfredpreferences/workflows/user.workflow.48975155-6FB6-4984-8E20-5207E1E28EF2/scripts/events/open.js
+++
@@ -1,9 +0,0 @@
-function run(argv) {
-	var query = argv[0];
-	var app = Application.currentApplication()
-	app.includeStandardAdditions = true
-	var Calendar = Application("Calendar")
-	var date = new Date(query)
-	Calendar.switchView({to: "day view"})
-	Calendar.viewCalendar({at: date})
-}
--- Alfred/Alfred.alfredpreferences/workflows/user.workflow.48975155-6FB6-4984-8E20-5207E1E28EF2/scripts/reminders/complete.js
+++
@@ -1,8 +0,0 @@
-function run(argv) {
-	var app = Application.currentApplication();
-	app.includeStandardAdditions = true;
-
-	const uuid = argv[0]
-
-	return app.doShellScript(`./agendas complete-by-uuid ${uuid}`)
-}
